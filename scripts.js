55d35601f4f7d727fb8b5c17ddf4a1f99a2fff61
infinuts <163611744+infinuts@users.noreply.github.com>
2024-06-18 21:06:07 +0100

    scripts.js

--- scripts.js
+++ scripts.js
@@ -1,20 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Get the modal element and the button to show the modal
     var modal = document.getElementById("script-modal");
     var btn = document.getElementById("show-script-btn");
     var closeBtn = document.querySelector(".close-button");
     var copyBtn = document.getElementById("copy-button");
     var scriptContent = document.getElementById("script-content");
 
-    // When the user clicks the button, display the modal
     btn.addEventListener("click", function() {
-        modal.style.display = "block";
-        // Ensure modal is centered
-        modal.style.justifyContent = "center";
-        modal.style.alignItems = "center";
+        modal.style.display = "flex";
     });
 
-    // When the user clicks on the close button or outside the modal, close it
     closeBtn.addEventListener("click", function() {
         modal.style.display = "none";
     });
@@ -25,17 +19,14 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 
-    // Copy script content to clipboard when copy button is clicked
     copyBtn.addEventListener("click", function() {
-        // Create a range object to select text
         var range = document.createRange();
         range.selectNode(scriptContent);
-        window.getSelection().removeAllRanges(); // Clear current selection
-        window.getSelection().addRange(range); // Add range to selection
-        document.execCommand("copy"); // Copy selected text
-        window.getSelection().removeAllRanges(); // Clear selection again
+        window.getSelection().removeAllRanges();
+        window.getSelection().addRange(range);
+        document.execCommand("copy");
+        window.getSelection().removeAllRanges();
     });
 
-    // Initially hide the script modal on page load
     modal.style.display = "none";
 });
